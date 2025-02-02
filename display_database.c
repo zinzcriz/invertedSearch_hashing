592b6f8fef5bbb0218db2075c3fa7735ea6fca00
Zinz Criz Xavier <crizxavier10@gmail.com>
2025-02-02 14:13:15 +0530

    Modified files

--- display_database.c
+++ display_database.c
@@ -1,53 +1,9 @@
 #include "inverted_search.h"
 extern int status;
-// int display_database(hash_t *arr,int size)
-// {
-//     /*if(status!=1)
-//     {
-//         return FAILURE;
-//     }*/
-//     struct main_node *m_link;
-//     struct sub_node *s_link;
-//     int flag=0;
-//     printf("index\t\tword\t\tfilecount\t\tfilename\t\twordcount\n");
-//     for(int i=0;i<size;i++)
-//     {
-//         if(arr[i].link!=NULL)
-//         {
-//             flag=1;
-//             m_link=arr[i].link;
-//             while(m_link!=NULL)
-//             {
-//                 s_link=m_link->sub_link;
-//                 printf("[%d]",i);
-//                 printf("\t\t");
-//                 printf("[%s]",m_link->word);
-//                 printf("\t\t");
-//                 printf("%d",m_link->file_count);
-//                 printf("\t\t");
-//                 printf("Files: ");
-//                 while(s_link!=NULL)
-//                 {
-//                     printf("%s  %d",s_link->filename,s_link->word_count);
-//                     printf("\t\t");
-//                     //printf("%d",s_link->word_count);
-//                     //printf("  ");
-//                     s_link=s_link->link;
-//                 }
-//                 m_link=m_link->main_link;
-//                 printf("\n");
-
-//             }
-//         }
-//     }
-//     if(flag==0)
-//         return FAILURE;
-// }
-#include "inverted_search.h"
-
+extern int update_status;
 int display_database(hash_t *HT)
 {
-    if (status != 1)
+    if (status != 1 && update_status!=1)
     {
         return FAILURE;
     }
